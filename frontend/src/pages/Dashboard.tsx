0b8babc6fd4bfe760d921d05124441e331e7b68c
Arjun Girish <avg2000@hw.ac.uk>
2024-01-08 22:22:56 +0400

    edited import message

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Divider, Box } from "@chakra-ui/react";
 import { motion, AnimatePresence } from "framer-motion";
-import NavBar from "../components/Dashboard/NavBar";
+import NavBar from "../components/Dashboard/Navbar";
 import Projects from "../components/Dashboard/Projects";
-import Profile from "../components/Dashboard/Profile";
+import Profile from "../components/Dashboard/profile";
 import SideBar from "../components/Dashboard/Sidebar";
 // import Trash from "./Trash";
-import Trash from "../components/Trash/TrashAlt";
+import Trash from "../components/Trash/trashAlt";
 
 const Dashboard: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
