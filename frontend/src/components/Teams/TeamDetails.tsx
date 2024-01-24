7626128324444a6fb2db0a380517855480ed0024
isa <isaisaev.ru@gmail.com>
2024-01-24 22:33:58 +0400

    added dashboard to teams chat

--- frontend/src/components/Teams/TeamDetails.tsx
+++ frontend/src/components/Teams/TeamDetails.tsx
@@ -75,7 +75,7 @@
 // TeamDetails.tsx
 
 import React, { useEffect, useState } from "react";
-import { Avatar, Flex, Box, Text, Stack, Badge } from "@chakra-ui/react";
+import { Avatar, Flex, Box, Text, Stack, Badge, Divider } from "@chakra-ui/react";
 import { db } from "../../firebase-config";
 import {
   doc,
@@ -87,11 +87,25 @@ import "./TeamDetails.scss"; // Import the SCSS file
 import { IoChatbubblesSharp } from "react-icons/io5";
 import { useNavigate, useParams } from "react-router-dom";
 import InvModal from "./InvModal";
+import Navbar from "../Dashboard/Navbar";
+import { AnimatePresence, motion } from "framer-motion";
+import SideBar from "../Dashboard/sidebar";
 
 const TeamDetails: React.FC = () => {
   const [teamDetails, setTeamDetails] = useState<DocumentData | null>(null);
   let { team_id } = useParams();
   const navigate = useNavigate();
+
+    // Dashboard routing
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+    const sidebarVariants = {
+      open: { width: "200px" },
+      closed: { width: "0px" },
+    };
+  
+    // Function to toggle the sidebar
+    const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
   if (team_id) {
     team_id = decodeURIComponent(team_id);
@@ -137,6 +151,51 @@ const TeamDetails: React.FC = () => {
   };
 
   return (
+    <>
+      <div style={{ padding: "10px", background: "#484c6c" }}>
+        <Navbar onToggle={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+      </div>
+      <Divider borderColor="lightgrey" borderWidth="1px" maxW="98.5vw" />
+      <Box display="flex" height="calc(100vh - 10px)">
+        <AnimatePresence>
+          {isSidebarOpen ? (
+            <motion.div
+              initial="open"
+              animate="open"
+              exit="closed"
+              variants={sidebarVariants}
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              style={{
+                paddingTop: "10px",
+                height: "inherit",
+                backgroundColor: "#f6f6f6",
+                boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
+                overflow: "hidden",
+              }}
+            >
+              <SideBar />
+            </motion.div>
+          ) : (
+            <motion.div
+              initial="closed"
+              animate="clsoed"
+              exit="open"
+              variants={sidebarVariants}
+              transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              style={{
+                paddingTop: "10px",
+                height: "inherit",
+                backgroundColor: "#f6f6f6",
+                boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
+                overflow: "hidden",
+              }}
+            >
+              <SideBar />
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <Box flexGrow={1} padding="10px" marginLeft={5}>
+
     <div className="team-details-container">
       {teamDetails ? (
         <div className="profile-container">
@@ -196,6 +255,9 @@ const TeamDetails: React.FC = () => {
         <p>Loading team details...</p>
       )}
     </div>
+        </Box>
+      </Box>
+    </>
   );
 };
 
