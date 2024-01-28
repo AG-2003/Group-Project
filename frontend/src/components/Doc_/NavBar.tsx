2bf50ccb8c2ca53e6ad02ae1419a761a6923cc4b
Arjun Girish <avg2000@hw.ac.uk>
2024-01-28 20:53:09 +0400

    FEAT: able to start a call in all 3 suites

--- frontend/src/components/Doc_/NavBar.tsx
+++ frontend/src/components/Doc_/NavBar.tsx
@@ -11,6 +11,11 @@ import {
 } from "react-icons/io5";
 import "./NavBar.scss";
 import { useNavigate } from "react-router-dom";
+import { Message } from "../../interfaces/Message";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase-config";
+import { serverTimestamp } from "firebase/firestore";
+
 
 interface Props {
   isSidebarOpen: boolean;
@@ -23,6 +28,7 @@ interface Props {
 
 const NavBar = ({ onToggle, isSidebarOpen, documentTitle, setDocumentTitle }: Props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [user] = useAuthState(auth);
   const navigate = useNavigate();
   const iconStyle = {
     transform: isSidebarOpen ? "rotate(90deg)" : "rotate(0deg)",
@@ -36,6 +42,52 @@ const NavBar = ({ onToggle, isSidebarOpen, documentTitle, setDocumentTitle }: Pr
       setDocumentTitle("Untitled");
     }
   };
+
+  function randomID(len: number) {
+    let result = "";
+    if (result) return result;
+    var chars =
+      "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
+      maxPos = chars.length,
+      i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+  }
+
+
+
+  // Function to start the call
+  const handleStartCall = () => {
+    // Generate the roomID and construct the meeting link
+    const roomID = randomID(5);
+    const meetingLink =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      "/meeting?roomID=" +
+      roomID;
+
+    // Navigate to the Zoom meeting page
+    navigate(`/meeting?roomID=${roomID}`);
+
+    // Now you can send the meeting link to the chat or use it as needed
+    // For example, you can add a new message to the chat
+    const newMessage: Message = {
+      id: Date.now().toString(),
+      text: meetingLink,
+      userId: user?.email,
+      timestamp: serverTimestamp(),
+      userPic: user?.photoURL,
+      userName: user?.displayName,
+    };
+
+    // Add the new message to the chat
+
+  };
+
 
   return (
     <div className="navbar">
@@ -79,7 +131,7 @@ const NavBar = ({ onToggle, isSidebarOpen, documentTitle, setDocumentTitle }: Pr
             className="action-icon call"
             aria-label="Video Call"
             icon={<IoVideocamOutline />}
-            onClick={onOpen}
+            onClick={handleStartCall}
           />
         </Tooltip>
         <Tooltip
